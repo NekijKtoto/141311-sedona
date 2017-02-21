33e2e26310fceb3f5ba89b92fd4079f5936098b9
Concinnity888 <pogorelova_ann@mail.ru>
2017-02-21 21:44:00 +0500

    Внесла правки

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
+document.body.classList.remove("no-js");
+
 var link = document.querySelector(".btn-search");
-var form = document.querySelector(".search-form form");
-link.addEventListener("click", function(){
+var form = document.querySelector(".search-form .form");
+
+link.addEventListener("click", function(event){
   event.preventDefault();
   form.classList.toggle("js-open-form");
 });
